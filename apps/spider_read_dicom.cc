0627bba1857334c08929c90241c30ac77aad36d3
Jake Forster <jake.forster@sa.gov.au>
2025-12-23 11:34:18 +1030

    spider_read_dicom: Prevent throwing filesystem error.
    Use non-throwing ctors for directory iterator.
    
    * apps/spider_read_dicom.cc (ReadDicomFileInDir): Prevent throwing
    filesystem error.

--- apps/spider_read_dicom.cc
+++ apps/spider_read_dicom.cc
@@ -1,13 +1,14 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 // Copyright (C) 2025 South Australia Medical Imaging
 
-#include <cstdlib> // EXIT_FAILURE, EXIT_SUCCESS
+#include <cstdlib> // EXIT_FAILURE, EXIT_SUCCESS, std::exit
 #include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <string>
 #include <string_view>
 #include <vector>
+#include <system_error>
 
 #include <gdcmDataSet.h>
 #include <gdcmReader.h>
@@ -22,19 +23,29 @@ bool
 ReadDicomFileInDir(const std::string_view dir,
                    std::filesystem::path& path_found, gdcm::Reader& r)
 {
-  for (const auto& f : std::filesystem::directory_iterator(dir))
+  std::error_code ec;
+  std::filesystem::directory_iterator it(dir, ec);
+  if (ec)
     {
-      if (!f.is_regular_file())
+      std::cerr << "Error: Cannot open directory '" << dir
+                << "': " << ec.message() << '\n';
+      std::exit(EXIT_FAILURE);
+    }
+  const std::filesystem::directory_iterator end{};
+  for (; it != end; ++it)
+    {
+      const std::filesystem::directory_entry& e = *it;
+      if (!e.is_regular_file())
         continue;
       // Use SetStream instead of SetFileName because filesystem::path
       // is wchar_t on Windows.
-      std::ifstream is(f.path(), std::ios::binary);
+      std::ifstream is(e.path(), std::ios::binary);
       if (!is)
         continue;
       r.SetStream(is);
       if (r.Read())
         {
-          path_found = f.path();
+          path_found = e.path();
           return true;
         }
     }
