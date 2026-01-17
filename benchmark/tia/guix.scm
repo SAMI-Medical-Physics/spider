f1ffe9b1b285045f9085314b87eaf41b86e0cc8e
Jake Forster <jake.forster@sa.gov.au>
2026-01-17 10:33:24 +1030

    benchmark: factor out spect-dicom-dir procedure.
    * benchmark/tia/guix.scm (spect-dicom-dir): New procedure.
    (spect): Use it.

--- benchmark/tia/guix.scm
+++ benchmark/tia/guix.scm
@@ -1,5 +1,5 @@
 ;;; SPDX-License-Identifier: AGPL-3.0-or-later
-;;; Copyright (C) 2025 South Australia Medical Imaging
+;;; Copyright (C) 2025, 2026 South Australia Medical Imaging
 
 (use-modules (guix gexp)
              (guix packages)
@@ -39,6 +39,10 @@
                        (invoke (string-append #$unzip "/bin/unzip")
                                #$spect/cts.zip "-d" #$output)))))
 
+(define (spect-dicom-dir n)
+  #~(string-append #$spect/cts "/patient_4/SPECT_Cts/scan"
+                   #$(number->string n) "/spect"))
+
 (define dcm2niix
   (specification->package "dcm2niix"))
 
@@ -52,10 +56,7 @@
                        (invoke (string-append #$dcm2niix "/bin/dcm2niix")
                                "-o" #$output
                                "-f" "spect"
-                               (string-append #$spect/cts
-                                              "/patient_4/SPECT_Cts/scan"
-                                              #$(number->string n)
-                                              "/spect"))))))
+                               #$(spect-dicom-dir n))))))
 
 (define elastix
   (specification->package "elastix"))
